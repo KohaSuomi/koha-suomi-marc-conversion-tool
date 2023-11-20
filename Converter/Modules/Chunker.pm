ba6977d2c04c4c2349ba6f282f2b29158ba66ecb
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2023-11-20 10:11:15 +0200

    Add script for printing MARC records to a file

---
+++ Converter/Modules/Chunker.pm
@@ -0,0 +1,159 @@
+package Converter::Modules::Chunker;
+#!/usr/bin/perl
+
+# Copyright Vaara-kirjastot 2015
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use C4::Record;
+use C4::Biblio;
+
+=head SYNOPSIS
+
+    BiblioChunker paginates database access to a large volume of marcxml from the DB.
+
+Loading a large amount of biblios (especially the marcxml-column) at once will have
+unexpected consequences. This object paginates a DB access to the biblio-table.
+
+=cut
+
+sub new {
+    my ($class, $startingBiblionumber, $endingBiblionumber, $pageSize, $verbose) = @_;
+    my $self = {};
+    $self->{startingBiblionumber} = $startingBiblionumber || 0;
+    $self->{endingBiblionumber} = $endingBiblionumber || 99999999999;
+    $self->{pageSize} = $pageSize  || 100;
+    $self->{position} = {
+        start => $self->{startingBiblionumber},
+        end => $self->{startingBiblionumber} + $self->{pageSize},
+        page => 1,
+    };
+    $self->{verbose} = $verbose || 0;
+    bless($self, $class);
+    return $self;
+}
+
+sub getChunkAsMARCRecord {
+    my ($self) = @_;
+    my $chunk = $self->_getChunk();
+    return $chunk unless $chunk;
+
+    for (my $i=0 ; $i<scalar(@$chunk) ; $i++) {
+        my $bi = $chunk->[$i];
+        my $marcxml = C4::Biblio::GetXmlBiblio($bi->{biblionumber});
+        my $error = "";
+        ($error, $chunk->[$i]) = C4::Record::marcxml2marc($marcxml);
+        die "Couldn't get MARC::Record for biblio $bi->{biblionumber}" if $error;
+        $chunk->[$i]->{biblionumber} = $bi->{biblionumber};
+        $chunk->[$i]->{biblioitemnumber} = $bi->{biblioitemnumber};
+        $chunk->[$i]->{frameworkcode} = $bi->{frameworkcode};
+    }
+    return $chunk;
+}
+
+sub getChunk {
+    my ($self) = @_;
+    return $self->_getChunk();
+}
+
+sub _getChunk {
+    my ($self) = @_;
+    my @cc = caller(0);
+
+    if ($self->{verbose} > 0) {
+        print ' #'.DateTime->now()->iso8601()."# ".$cc[3]." is getting new chunk ".$self->{position}->{page}.", ".$self->{position}->{start}."-".$self->{position}->{end}." #\n" if $self->{verbose} > 0;
+    }
+
+    unless ($self->_isChunkWithinBounds()) {
+        return undef;
+    }
+    my $dbh = C4::Context->dbh();
+    my $query = "(
+        SELECT b.biblionumber, b.timestamp, b.frameworkcode, 0 as deleted FROM biblio b
+        WHERE b.biblionumber >= ? AND b.biblionumber < ?
+    )";
+    my $sth = $dbh->prepare($query);
+    $sth->execute( $self->_getPosition() );
+    if ($sth->err) {
+        die $cc[3]."():> ".$sth->errstr;
+    }
+    my $chunk = $sth->fetchall_arrayref({});
+    if (ref $chunk eq 'ARRAY' && scalar(@$chunk)) {
+        $self->_incrementPosition();
+        return $chunk;
+    }
+    else {
+        my $nextAvailableBiblioitemnumber = $self->_getNextId();
+        if ($nextAvailableBiblioitemnumber) {
+            $self->_incrementPosition($nextAvailableBiblioitemnumber);
+            return $self->getChunk();
+        }
+        else {
+            return undef;
+        }
+    }
+    return (ref $chunk eq 'ARRAY' && scalar(@$chunk)) ? $chunk : undef;
+}
+
+sub _getPosition {
+    my ($self) = @_;
+    return ($self->{position}->{start}, $self->{position}->{end});
+}
+sub _incrementPosition {
+    my ($self, $newStart) = @_;
+    if ($newStart) {
+        $self->{position}->{start} = $newStart;
+        $self->{position}->{end}   = $self->{position}->{start} + $self->{pageSize};
+    }
+    else {
+        $self->{position}->{start} += $self->{pageSize};
+        $self->{position}->{end}   += $self->{pageSize};
+    }
+    $self->{position}->{page}++;
+}
+sub _getNextId {
+    my ($self) = @_;
+
+    my $dbh = C4::Context->dbh();
+    my $sth = $dbh->prepare("SELECT MIN(biblionumber) FROM biblio WHERE biblionumber > ?");
+    my @pos = $self->_getPosition();
+    $sth->execute( $pos[0] );
+    if ($sth->err) {
+        my @cc = caller(0);
+        die $cc[3]."():> ".$sth->errstr;
+    }
+    my ($biblionumber) = $sth->fetchrow();
+    return $biblionumber;
+}
+sub _isChunkWithinBounds {
+    my ($self) = @_;
+
+    if ($self->{endingBiblionumber} < $self->{position}->{end}) {
+        if ($self->{endingBiblionumber} > ($self->{position}->{end} - $self->{pageSize})) {
+            $self->{position}->{end} = $self->{endingBiblionumber};
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    else {
+        return 1;
+    }
+}
+1;
---
+++ Converter/scripts/print_marcs.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl
+
+# Copyright 2023 Koha-Suomi Oy
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+
+BEGIN {
+    # find Koha's Perl modules
+    # test carefully before changing this
+    use FindBin;
+    eval { require "$FindBin::Bin/../kohalib.pl" };
+}
+
+use Getopt::Long;
+use XML::LibXML;
+
+use C4::Record qw( marc2marcxml );
+
+use Converter::Modules::Chunker;
+
+use open ':std', ':encoding(UTF-8)';
+
+sub usage {
+    print STDERR <<USAGE;
+  Will print all active MARC records in database to
+  OUTPUT_DIRECTORY .
+
+  -p --path
+  -l --limit
+  -v --verbose
+
+USAGE
+    exit $_[0];
+}
+
+my ( $help, $config, $path, $limit, $verbose );
+
+GetOptions(
+    'h|help'       => \$help,
+    'p|path:s'     => \$path,
+    'l|limit:i'    => \$limit,
+    'v|verbose'  => \$verbose,
+) || usage(1);
+
+usage(0) if ($help);
+
+if ( !$path || !-d $path || !-w $path ) {
+    print STDERR
+"Error: You must specify a valid and writeable directory to dump the print notices in.\n";
+    usage(1);
+}
+
+my $count = 0;
+my $chunker = Converter::Modules::Chunker->new(undef, $limit, undef, $verbose);
+while (my $records = $chunker->getChunkAsMARCRecord(undef, undef)) {
+    my $xml = MARC::File::XML::header('UTF-8');
+    my $timestamp = POSIX::strftime( "%Y%m%d%H%M%S", localtime );
+    $count++;
+    my $records_count = 0;
+    my $filename = "MARCrecords_".$timestamp."_".$count;
+
+    foreach my $record (@$records) {
+        eval {
+            #fetch and parse records
+            my $marc_xml = marc2marcxml($record, 'UTF-8', C4::Context->preference("marcflavour"));
+            my $parser = XML::LibXML->new(recover => 1);
+            my $doc = $parser->load_xml(string => $marc_xml);
+            my ( $row ) = $doc->findnodes("/*");
+            #add records to new xml file
+            $xml .= $row."\n";
+            $records_count++;
+        };
+        warn $@ if $@;
+    }
+
+    #send file to output directory
+    open(my $fh, '>', $path.$filename);
+    print $fh $xml;
+    close $fh;
+    print "Added ".$records_count." records to file ".$filename.".\n" if $verbose;
+}
