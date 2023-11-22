6d2b1015fc3c0dccd8749155d513944774a90394
Johanna Raisa <johanna.raisa@gmail.com>
2023-11-22 12:41:29 +0200

    Update UsemarconConverter.pm to use project root path

--- Converter/Modules/UsemarconConverter.pm
+++ Converter/Modules/UsemarconConverter.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use IPC::System::Simple qw(capture);
 use File::Path qw(make_path);
+use FindBin '$Bin';
 
 sub new {
     my ($class, $params) = @_;
@@ -21,13 +22,15 @@ sub verbose {
 sub convertRecords {
     my ($self, $input_path, $input_file, $usemarcon_config) = @_;
 
+    my $project_root = "$Bin/../..";  # This will give you the project root path
+
     my $output_path = $input_path."/rda/";
     my $output_file = $input_file.".rda";
 
     # Create the output directory if it does not exist
     make_path($output_path);
 
-    my $converted_record = capture("../../usemarcon/program/usemarcon", $usemarcon_config, $input_path.$input_file, $output_path.$output_file);
+    my $converted_record = capture("$project_root/usemarcon/program/usemarcon", $usemarcon_config, $input_path.$input_file, $output_path.$output_file);
     
     print "$converted_record\n" if $self->verbose;
     return ($output_path, $output_file);
