dd93079fd744a9d07b57b8ce5e7b84ca7ba5c647
Johanna Räisä <johanna.raisa@gmail.com>
2024-09-30 13:34:42 +0300

    Change script content

--- Converter/scripts/compare_biblios_on_action_log.pl
+++ Converter/scripts/compare_biblios_on_action_log.pl
@@ -8,42 +8,36 @@ use C4::Context;
 use C4::Biblio qw( ModBiblio GetFrameworkCode);
 
 
-my $date = '';
+my $starttimestamp = '';
+my $endtimestamp = '';
 my $confirm = 0;
 my $verbose = 0;
 GetOptions(
-    'date=s' => \$date,
+    'starttimestamp=s' => \$starttimestamp,
+    'endtimestamp=s' => \$endtimestamp,
     'confirm' => \$confirm, 
     'verbose' => \$verbose) 
-    or die "Usage: $0 --date=YYYY-MM-DD [--confirm] [--verbose]\n";
+    or die "Usage: $0 --starttimestamp=YYYY-MM-DD --endtimestamp=YYYY-MM-DD [--confirm] [--verbose]\n";
 
-if (!$date) {
-    die "Usage: $0 --date=YYYY-MM-DD [--confirm] [--verbose]\n";
+if (!$starttimestamp || !$endtimestamp) {
+    die "Usage: $0 --starttimestamp=YYYY-MM-DD --endtimestamp=YYYY-MM-DD [--confirm] [--verbose]\n";
 }
 
 my $dbh = C4::Context->dbh;
-my $sth = $dbh->prepare("SELECT marcxml, upload_timestamp FROM import_records WHERE DATE(upload_timestamp) >= ?");
-my $res = $sth->execute($date);
+my $query = "select object from action_logs where module = 'CATALOGUING' and action = 'MODIFY' and info like 'biblio%' and timestamp >= ? and timestamp <= ? group by object having count(object) > 1; ";
+print "$query\n";
+my $sth = $dbh->prepare($query);
+my $res = $sth->execute($starttimestamp, $endtimestamp);
 my $count = 0;
 
 while (my $row = $sth->fetchrow_hashref) {
-    my $marcxml = MARC::Record::new_from_xml($row->{marcxml}, 'UTF-8');
-    my $biblionumber = $marcxml->field('999')->subfield('c');
-    my $log = Koha::ActionLogs->search(
-        {
-            module => 'CATALOGUING',
-            action => 'MODIFY',
-            object => $biblionumber,
-            timestamp => { -between => [ $date.' 06:00:00', $row->{upload_timestamp} ] },
-        },
-    )->unblessed;
-    
-    if (scalar(@$log) > 1) {
-        foreach my $log_entry (@$log) {
-            print "Biblionumber: $biblionumber, Timestamp: $log_entry->{timestamp}\n";
-            $count++;
-        }
+    my $biblionumber = $row->{object};
+    print "Biblionumber $biblionumber has multiple MODIFY actions\n";
+    if ($confirm) {   
+        $dbh->do("update biblio_metadata set timestamp = NOW() where biblionumber = ?", undef, $biblionumber);
+        print "Updated biblionumber $biblionumber\n";
     }
+    $count++;
 }
 
 print "Total: $count\n";
