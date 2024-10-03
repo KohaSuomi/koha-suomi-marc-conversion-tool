6bf16bbe0d445e201de4cfc6e7dacd5f257649f9
Johanna Räisä <johanna.raisa@gmail.com>
2024-10-03 14:41:02 +0300

    Find max match imported

--- Converter/scripts/last_imported.pl
+++ Converter/scripts/last_imported.pl
@@ -7,34 +7,22 @@ use Koha::Import::Records;
 
 my $dbh = C4::Context->dbh;
 # Query to find the last imported batch
-my $sql = "SELECT import_batch_id, upload_timestamp 
+my $sql = "SELECT MAX(matched_biblionumber) AS matched_biblionumber
            FROM import_batches 
+           JOIN import_records ON import_batches.import_batch_id = import_records.import_batch_id
+           JOIN import_biblios ON import_records.import_record_id = import_biblios.import_record_id
            WHERE import_status = 'imported' 
            AND file_name LIKE '\%MARCrecordsChunk\%' 
-           AND upload_timestamp >= DATE_SUB(CURDATE(), INTERVAL 1 DAY) 
-           ORDER BY upload_timestamp DESC 
-           LIMIT 1";
+           AND import_batches.upload_timestamp >= DATE_SUB(CURDATE(), INTERVAL 1 DAY) 
+           ORDER BY import_batches.upload_timestamp DESC";
 
 # Prepare and execute the query
 my $sth = $dbh->prepare($sql);
 $sth->execute();
 
 # Fetch the result
-my ($import_batch_id, $upload_timestamp) = $sth->fetchrow_array();
-my $import_records = Koha::Import::Records->search({
-        import_batch_id => $import_batch_id,
-    });
-
-# Loop through the records
-my $next_biblionumber = 0;
-while ( my $record = $import_records->next ) {
-    # Do something with the record
-    my $matched_biblionumber = $record->import_biblio->matched_biblionumber;
-    if ($matched_biblionumber) {
-        $next_biblionumber = $matched_biblionumber;
-    }
-}
-# Clean up
+my ($matched_biblionumber) = $sth->fetchrow_array();
 $sth->finish();
-$next_biblionumber++;
-print $next_biblionumber;
+$matched_biblionumber = 0 unless $matched_biblionumber;
+$matched_biblionumber++;
+print $matched_biblionumber;
